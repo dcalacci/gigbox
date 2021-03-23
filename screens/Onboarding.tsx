682735c3d2a165ef70e22add10bf64cc03358b5f
Dan Calacci <dcalacci@media.mit.edu>
2021-03-23 18:36:12 -0400

    Splits OTP logic into its own slice

--- screens/Onboarding.tsx
+++ screens/Onboarding.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, ScrollView, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { tailwind } from "tailwind";
-import PhoneEntry from "../components/PhoneEntry"
+import PhoneEntry from "../features/auth/PhoneEntry"
 
 export default function OnboardingScreen() {
   return (
