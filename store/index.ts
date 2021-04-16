f701b83fc9f0664eb8e0ca22efbaf860f14fdcec
Dan Calacci <dcalacci@media.mit.edu>
2021-04-16 12:32:08 -0400

    removes shiftlistslice (deprecated redux code)

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthSlice from '../features/auth/authSlice';
 import OtpSlice from '../features/auth/otpSlice';
-import ShiftListSlice from '../features/shiftList/shiftListSlice';
 
 const rootReducer = combineReducers({
     auth: AuthSlice,
     otp: OtpSlice,
-    shiftList: ShiftListSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
