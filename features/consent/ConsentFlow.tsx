e523a261fea2d05ded5033b858e02e7e209b9ec7
Dan Calacci <dcalacci@media.mit.edu>
2021-05-13 14:59:21 -0400

    Refactors Qs to be separate component
    - adds 'extras' consent screen

--- features/consent/ConsentFlow.tsx
+++ features/consent/ConsentFlow.tsx
@@ -16,84 +16,7 @@ import moment from 'moment';
 import { Job, Screenshot, Shift } from '../../types';
 import { parse } from 'wellknown';
 import TripMap from '../shiftList/TripMap';
-import { Ionicons } from '@expo/vector-icons';
-import * as Haptics from 'expo-haptics';
-
-export const BinarySurveyQuestion = ({
-    onPress,
-    declineText,
-    questionText,
-}: {
-    onPress: (yes: boolean) => void;
-    declineText?: string;
-    questionText?: string;
-}) => {
-    const [pressed, setPressed] = useState<boolean>();
-    const onPressButton = (yes: boolean) => {
-        setPressed(yes);
-        onPress(yes);
-        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-    };
-    return (
-        <View style={tailwind('rounded-lg bg-white p-2')}>
-            <Text style={tailwind('text-lg pt-2 pb-2 underline text-center')}>{questionText}</Text>
-
-            <View style={tailwind('flex-row flex-grow content-around p-2 w-full')}>
-                <Pressable
-                    onPress={() => onPressButton(false)}
-                    style={[
-                        tailwind('rounded-lg flex-grow m-2 p-2 '),
-                        pressed === false
-                            ? tailwind('bg-gray-800')
-                            : tailwind('border-2 border-gray-800'),
-                    ]}
-                >
-                    <Text
-                        style={[
-                            tailwind('text-lg text-white font-bold text-center'),
-                            pressed === false ? tailwind('text-white') : tailwind('text-gray-500'),
-                        ]}
-                    >
-                        <Ionicons
-                            size={24}
-                            name={'close-circle-outline'}
-                            color={pressed === false ? 'white' : 'gray'}
-                        />
-                        No
-                    </Text>
-                </Pressable>
-                <Pressable
-                    onPress={() => onPressButton(true)}
-                    style={[
-                        tailwind('rounded-lg flex-grow m-2 p-2'),
-                        pressed ? tailwind('bg-green-500') : tailwind('border-2 border-green-500'),
-                    ]}
-                >
-                    <Text
-                        style={[
-                            tailwind('text-lg text-white font-bold text-center'),
-                            pressed ? tailwind('text-white') : tailwind('text-green-500'),
-                        ]}
-                    >
-                        <Ionicons
-                            size={24}
-                            name={'checkmark-circle-outline'}
-                            color={pressed ? 'white' : 'green'}
-                        />
-                        Yes
-                    </Text>
-                </Pressable>
-            </View>
-            {pressed === false ? (
-                <View style={tailwind('p-2 ')}>
-                    <Text style={tailwind('text-red-500 font-bold text-center')}>
-                        {declineText}
-                    </Text>
-                </View>
-            ) : null}
-        </View>
-    );
-};
+import BinarySurveyQuestion from './BinarySurveyQuestion';
 
 export const DataConsentSection = ({
     sectionTitle,
@@ -131,7 +54,7 @@ export const ConsentFlow = ({}) => {
     return (
         <ScrollView style={tailwind('bg-gray-100 ')}>
             <View style={tailwind('p-2')}>
-                <Text style={[tailwind('text-2xl font-bold text-green-500')]}>
+                <Text style={[tailwind('text-3xl font-bold text-green-500')]}>
                     By using gigbox, you consent to being part of an MIT research study.
                 </Text>
 
